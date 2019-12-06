b8362d760d8b298a0fc6186ff99ba0dc4d70bb1c
Kevin Dixon <gaiwecoor@gmail.com>
2019-12-06 09:00:55 -0600

    Remove unnecessary logs, include `require` in setup

--- run.js
+++ run.js
@@ -1,21 +1,16 @@
 const day = process.argv[2];
 const {Solution} = require("./tools");
 
-const parts = require(`./solutions/day${day.padStart(2, "0")}`);
-
-const solve = new Solution(day, parts);
-
 console.time("Complete Time");
 console.time("Setup Time");
+const parts = require(`./solutions/day${day.padStart(2, "0")}`);
+const solve = new Solution(day, parts);
 solve.setup();
 console.timeEnd("Setup Time");
-console.log();
 console.time("Part 1 Time");
 solve.pt1();
 console.timeEnd("Part 1 Time");
-console.log()
 console.time("Part 2 Time");
 solve.pt2();
 console.timeEnd("Part 2 Time");
-console.log();
 console.timeEnd("Complete Time");
