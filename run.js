fd6b66ce32a364e2a1812c59e1d683095dfd7ff3
Kevin Dixon <gaiwecoor@gmail.com>
2019-12-17 10:25:22 -0600

    Async main, because I might need that some day?

--- run.js
+++ run.js
@@ -1,16 +1,20 @@
 const day = process.argv[2];
 const {Solution} = require("./tools");
 
-console.time("Complete Time");
-console.time("Setup Time");
-const parts = require(`./solutions/day${day.padStart(2, "0")}`);
-const solve = new Solution(day, parts);
-solve.setup();
-console.timeEnd("Setup Time");
-console.time("Part 1 Time");
-solve.pt1();
-console.timeEnd("Part 1 Time");
-console.time("Part 2 Time");
-solve.pt2();
-console.timeEnd("Part 2 Time");
-console.timeEnd("Complete Time");
+async function main() {
+  console.time("Complete Time");
+  console.time("Setup Time");
+  const parts = require(`./solutions/day${day.padStart(2, "0")}`);
+  const solve = new Solution(day, parts);
+  await solve.setup();
+  console.timeEnd("Setup Time");
+  console.time("Part 1 Time");
+  await solve.pt1();
+  console.timeEnd("Part 1 Time");
+  console.time("Part 2 Time");
+  await solve.pt2();
+  console.timeEnd("Part 2 Time");
+  console.timeEnd("Complete Time");
+}
+
+main();
